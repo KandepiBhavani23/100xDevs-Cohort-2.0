94a13c80a7c94778d12086dc4992a5d4fbec7f36
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-12-04 11:59:03 +0530

    Week - 1

---
+++ 0-1_Cohort/README.md
@@ -0,0 +1,3 @@
+# Steps to Run Test Cases
+
+1. npx jest ./tests/findLargestElement.test.js
--- Assignments/Week-1/easy/anagram.js
+++ Assignments/Week-1/easy/anagram.js
@@ -3,9 +3,14 @@
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
-
+const string1 = "School MASTER";
+const string2 = "The ClassROOM";
 function isAnagram(str1, str2) {
-
+  let string1 = str1.toLowerCase().split("").sort().join("");
+  let string2 = str2.toLowerCase().split("").sort().join("");
+  return string1 === string2;
 }
+
+console.log(isAnagram(string1, string2));
 
 module.exports = isAnagram;
