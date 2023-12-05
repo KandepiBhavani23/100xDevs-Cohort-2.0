f23da4193e327c7253b46491d86c0dbddaaa112e
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-12-05 19:40:43 +0530

    patterns

---
+++ JavaScript_Coding/Patterns.js/Pattern1.js
@@ -0,0 +1,18 @@
+/*
+
+
+ *
+ * *
+ * * *
+ * * * *
+ * * * * *
+ 
+ */
+
+for (let i = 1; i <= 5; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += "* ";
+  }
+  console.log(row);
+}
