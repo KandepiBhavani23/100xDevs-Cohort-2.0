ea03212d56875a294e867e8c859b628e7aeb6c6e
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-12-05 11:46:00 +0530

    palindrome.js

--- Assignments/Week-1/medium/palindrome.js
+++ Assignments/Week-1/medium/palindrome.js
@@ -3,8 +3,15 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+const originalStr = "A man a plan a canal Panama";
 function isPalindrome(str) {
-  return true;
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reversedStr = str.split("").reverse().join("");
+  console.log(str, reversedStr);
+
+  return str === reversedStr;
 }
+
+console.log(isPalindrome(originalStr));
 
 module.exports = isPalindrome;
