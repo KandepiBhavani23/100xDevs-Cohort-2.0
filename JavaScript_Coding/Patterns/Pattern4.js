e09b4be6bd270d93ca6c36e4f2a01fc5862656b6
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-12-06 09:09:16 +0530

    pattern4

---
+++ JavaScript_Coding/Patterns/Pattern4.js
@@ -0,0 +1,14 @@
+let n = 5;
+let res = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    if (j <= n - i) {
+      res += "  ";
+    } else {
+      res += "* ";
+    }
+  }
+  res += "\n";
+}
+
+console.log(res);
