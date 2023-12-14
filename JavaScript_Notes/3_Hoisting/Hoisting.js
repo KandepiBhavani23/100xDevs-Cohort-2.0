ef277d43116bd56557856b8a8dd16b090e656fb2
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-12-14 19:22:13 +0530

    Hoisting.js

--- JavaScript_Notes/3_Hoisting/Hoisting.js
+++ JavaScript_Notes/3_Hoisting/Hoisting.js
@@ -18,13 +18,13 @@ var b = 15;
 console.log(window.a); // undefined
 console.log(window.b); // 15
 
-let a1;
-a1 = 10;
-console.log(a1) // 10. Note declaration and assigning of a is in different lines.
+// let a1;
+// a1 = 10;
+// console.log(a1) // 10. Note declaration and assigning of a is in different lines.
 
-const b1;
-b1 = 10;
-console.log(b1); // SyntaxError: Missing initializer in const declaration.(This type of declaration won't work with const. const b = 10 only will work)
+// const b1;
+// b1 = 10;
+// console.log(b1); // SyntaxError: Missing initializer in const declaration.(This type of declaration won't work with const. const b = 10 only will work)
 
-const b3 = 100;
-b3 = 1000; //this gives us TypeError: Assignment to constant variable.
+// const b3 = 100;
+// b3 = 1000; //this gives us TypeError: Assignment to constant variable.
