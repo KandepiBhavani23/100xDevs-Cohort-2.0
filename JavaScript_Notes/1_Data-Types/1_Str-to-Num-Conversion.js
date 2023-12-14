fa3f662715f45bd635ecadb7701e5b3939144cec
Kandepi Bhavani <120291769+KandepiBhavani23@users.noreply.github.com>
2023-12-14 17:31:34 +0530

    Str-Num-Conversion.js

--- JavaScript_Notes/1_Data-Types/1_Str-to-Num-Conversion.js
+++ JavaScript_Notes/1_Data-Types/1_Str-to-Num-Conversion.js
@@ -9,3 +9,13 @@ console.log(typeof valueInNumber);
 let valueInString = Number(number1);
 console.log(valueInString); // NaN
 console.log(typeof valueInString); //will convert to Number but not a pure Number type
+
+try {
+  nonHoistedFunction(); // TypeError: nonHoistedFunction is not a function
+} catch (error) {
+  console.error(error.message);
+}
+
+var nonHoistedFunction = function () {
+  console.log("I am not hoisted!");
+};
